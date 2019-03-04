fef1d2e4ecb5a7fae41c87819d5b9f99ba810c7b
jaminyah <jaminyah@gmail.com>
2019-03-04 13:03:58 -0500

    Update storyboard and Readme file.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ScrollDynamic
+
+ScrollDynamic is a Swift 4.2, iOS application that demonstrates how to configure a scroll view with varying size content view. For this example, UILabels are used to contain texts of varying length.
+
+<p align="center">
+  <img src="/images/scroll.png" alt="Screen shot." /> 
+</p>
+
--- ScrollDynamic/ViewController.swift
+++ ScrollDynamic/ViewController.swift
@@ -13,21 +13,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var instructLabel: UILabel!
     @IBOutlet weak var eventLabel: UILabel!
+    @IBOutlet weak var areaLabel: UILabel!
+    @IBOutlet weak var headlineLabel: UILabel!
+    @IBOutlet weak var descripLabel: UILabel!
+    @IBOutlet weak var alertIDLabel: UILabel!
     
+    @IBOutlet weak var senderLabel: UILabel!
+    @IBOutlet weak var beginLabel: UILabel!
+    @IBOutlet weak var endLabel: UILabel!
+    @IBOutlet weak var urgencyLabel: UILabel!
+    @IBOutlet weak var severityLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        instructLabel.text = "Lorem ipsum dolor sit er elit lamet.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
+        eventLabel.text = "Flood Warning"
         
+        instructLabel.text = "PRECAUTIONARY/PREPAREDNESS ACTIONS...\n\nSAFETY MESSAGE...\nStay tuned to developments by listening to NOAA Weather radio."
         instructLabel.sizeToFit()
         
-        eventLabel.text = "Flood Warning"
-        /*
-        eventLabel.sizeToFit()
-       */
-       
+        areaLabel.text = "Mobile, AL"
+        headlineLabel.text = "Flood Warning issued January 15 at 9:06AM CST expiring January 16 at 3:00AM CST by NWS Mobile AL"
+        descripLabel.text = "Bayou Sara At Saraland affecting Mobile County.\n\n.Recent heavy rains have caused significant rises along the river.\nThe Flood Warning continues for\nthe Bayou Sara At Saraland\n* until late tonight.\n* At 9 AM Tuesday the stage was 4.5 feet.\n* Minor flooding is occurring and Minor flooding is forecast.\n* Flood stage is 4 feet.\n* The river will continue to fall to below flood stage by tonight.\n* At 4 feet...Flooding of streets begins."
+        descripLabel.sizeToFit()
+        alertIDLabel.text = "NWS-IDP-PROD-3318489-2899512"
+        
+        imageView.image = UIImage(imageLiteralResourceName: "dia_warn_flood")
+        senderLabel.text = "NWS Mobile AL"
+        beginLabel.text = "Begin: Jan 15, 2019"
+        endLabel.text = "End: Jan 16, 2019"
+        urgencyLabel.text = "Urgency: Immediate"
+        severityLabel.text = "Severity: Severe"
+        
     }
 
 }
